9798d3ef13baae2123139fff758da3a44beee7b7
danel-bbk <dlafuente003@gmail.com>
2023-07-06 17:26:32 +0200

    andaluh

--- chatbot.js
+++ chatbot.js
@@ -1,5 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 import dotenv from 'dotenv';
+import andaluh from "@andalugeeks/andaluh";
+
 dotenv.config();
 
 
@@ -13,15 +15,19 @@ async function getResponse(message){
           
           
         const openai = new OpenAIApi(configuration);
+
+        const andaluhEPA = new andaluh.EPA();
         
-        const context = "te llamas Nacho, eres sevillano y eres el que gestiona el márketing de Alex O'Dogherty. Tienes que responder a todos los mensajes con datos curiosos sobre Alex (pueden ser inventados) o recordando que eres 'Nacho el de Alex' no digas hola ni preguntes si puedes ayudar en algo.";
+        const context = "te llamas Nacho, eres sevillano y eres el que gestiona el márketing de Alex O'Dogherty. Tienes que responder a todos los mensajes con datos curiosos sobre Alex (pueden ser inventados) o recordando que eres 'Nacho el de Alex' no digas hola ni preguntes si puedes ayudar en algo. Debes hacer mucho énfasis en que eres sevillano, tanto en la forma de escribir ( como hablan los sevillanos, cambiando las s por h), como en las expresiones que uses.";
           
         const chatCompletion = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [{role:"system",content:context},{role: "user", content: message}],
         });
-    
-        return chatCompletion.data.choices[0].message.content;
+
+        let texto = chatCompletion.data.choices[0].message.content;
+        return andaluhEPA.transcript(texto)
+        
     }
     catch(e){
         console.log(e);
